9b6770a4ee9db5ecb907005ec1bcbfed8174064b
liruHuang <liruHuang@gmail.com>
2024-12-20 17:58:45 +0800

    Kotlin readLine()

--- app/src/main/java/com/annie/bmi/Guessing.kt
+++ app/src/main/java/com/annie/bmi/Guessing.kt
@@ -1,8 +1,22 @@
 package com.annie.bmi
 
 fun main() {
-    print("please enter a number (1-10): " )
-    val input = readLine()
-    val num = input?.toIntOrNull() ?: 0
-    println("The number you enter is $num ")
+    val secret = 4
+    val num =0
+    while(num != secret){
+        print("please enter a number (1-10): " )
+        val input = readLine()
+        val num = input?.toIntOrNull() ?: 0
+        println("The number you enter is $num ")
+        if(num > secret){
+            println("lower")
+        }else if(secret > num){
+            println("higher")
+        }else {
+            println("Success!! the number is $secret")
+            break
+        }
+
+    }
+
 }
